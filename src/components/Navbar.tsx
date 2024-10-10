5f07b4c5f8fd022552cf5d1cc1ff5686a7230090
jkam <jpkamdem@proton.me>
2024-10-10 19:37:32 +0200

    pas mal d'avancée, galère un peu avec les states et la classe dynamique qui s'active pas

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,32 @@
 import styles from "./navbar.module.css";
+import { Link } from "react-router-dom";
+import logo from "../assets/hakushin_logo.svg";
+import burgerMenu from "../assets/menu.svg";
 
-export default function Navbar() {
+interface Props {
+  isOpen: boolean;
+  setToggleView: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function Navbar({ isOpen, setToggleView }: Props) {
+  function handleView() {
+    setToggleView(!isOpen);
+  }
+
   return (
     <>
-      <nav className={styles.nav}>nav</nav>
+      <nav className={styles.nav}>
+        <Link to="/">
+          <img className={styles["nav-logo-container"]} src={logo} alt="" />
+        </Link>
+        <p>Hakush.in</p>
+        <img
+          onClick={handleView}
+          className={styles["burger-menu"]}
+          src={burgerMenu}
+          alt=""
+        />
+      </nav>
     </>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom";
 import styles from "./sidebar.module.css";
+import logo from "../assets/hakushin_logo.svg";
+import homeLogo from "../assets/CommonTabIcon.webp";
 
-export default function Sidebar() {
+interface Props {
+  isOpen: boolean;
+}
+
+export default function Sidebar({ isOpen }: Props) {
   return (
     <>
-      <aside className={styles.aside}>aside</aside>
+      <aside className={styles.aside}>
+        <div className={styles["logo-container"]}>
+          <Link to="/">
+            <img className={styles.logo} src={logo} alt="Hakushin icon" />
+          </Link>
+        </div>
+
+        <ul className={`${styles.navlink} ${isOpen ? styles.show : ""}`}>
+          <Link to="/">
+            <img className={styles["nav-logo"]} src={homeLogo} alt="" />
+            Home
+          </Link>
+        </ul>
+      </aside>
     </>
   );
 }
